21ed7202159abc5746c3a9d744c92bd78bedb6e9
Yoyoeye <yoyoeye@yoyoeye-TravelMate-2480.(none)>
2017-12-31 18:22:52 +0200

    app addet

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './css/App.css';
+import Clock from './Clock';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'December 31, 2017',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline(){
+        this.setState({deadline: this.state.newDeadline})
+    }
+
+    render(){
+        return(
+        <div className="App">
+            <div className="App-title">
+                Counter to {this.state.deadline}
+            </div>
+            <Clock
+                deadline={this.state.deadline}
+            />
+            <div className="Input">
+                <input className="Input-form" placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})}
+                />
+                <button className="Button" type="submit" onClick={() => this.changeDeadline()}>Submit</button>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default App;
