3b85038c13eb1a0aeb714cca9db89304d9faa898
Orest <Joebereza@gmail.com>
2017-12-31 18:43:22 +0200

    date changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            deadline: 'December 31, 2017',
+            deadline: 'January 01, 2018',
             newDeadline: ''
         }
     }
